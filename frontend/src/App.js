4d7eb998c001989804bc8f0e9ae4095844b6e86b
Nikita <kukushkinnika@gmail.com>
2023-01-25 16:14:35 -0500

    Deleted h2 tag

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -102,10 +102,6 @@ function App() {
           To Read!
         </h1>
 
-        <h2 className="ftext-lg font-medium text-gray-900 dark:text-white">
-          Let's add some books!
-        </h2>
-
         <button
           onClick={showAddBook}
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
