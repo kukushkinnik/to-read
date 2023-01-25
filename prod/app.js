d8900b64a7a27ab1622670e4d8968e909aa58c86
Nikita <kukushkinnika@gmail.com>
2023-01-25 16:16:12 -0500

    Connected frontend with backend

---
+++ prod/app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const requestLogger = require("./utils/middleware");
+const express = require("express");
+const bookRoutes = require("./controllers/book");
+const { MONGODB_URI } = require("./utils/config");
+const cors = require("cors");
+const app = express();
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+app.use(cors());
+
+app.use(express.static("build"));
+app.use(express.json());
+app.use(requestLogger);
+app.use("/api/books", bookRoutes);
+
+module.exports = app;
